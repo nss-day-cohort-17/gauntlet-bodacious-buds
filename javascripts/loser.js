0cb099a1702f495dcd31aebc10442c068ff592d1
T-Farmer <terrill@Rels-Pro-186.local>
2016-12-15 09:15:27 -0600

    deleted loser.js

--- javascripts/loser.js
+++
@@ -1,7 +0,0 @@
-// As soon as either combatant reaches health of 0,
-// disable the attack button and announce the winner.
-
-if(Math.floor(this.health.val = 0)){
-      alert("May tales of your exploits live forever...you died, though.");
-      return;
-    };
