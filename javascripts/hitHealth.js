54bd48a1d8cd8522a5d721e6ef58050726a0c307
T-Farmer <terrill@Rels-Pro-186.local>
2016-12-14 19:58:48 -0600

    worked on damage in index2 and hitHealth

---
+++ javascripts/hitHealth.js
@@ -0,0 +1,17 @@
+//damage functionality for health progress bar
+
+// $(document).ready(function() {
+//   console.log("Let's go!!");
+
+//   var amount = 100;
+
+//   function attack(e){
+//     $('.hit').on('click', function(){
+//       amount = amount - Math.floor(Math.random);
+//       $('.progressValue').css('width', amount + '%')
+//     });
+//   }
+
+//   attack();
+
+// })
